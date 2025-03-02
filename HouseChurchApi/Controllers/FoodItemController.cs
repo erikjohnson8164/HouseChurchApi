d7a77c59f1de4f844c9a60097556e5fd687ee995
ejohnson-tp <erikanthonyjohnson@gmail.com>
2025-03-02 10:33:40 -0800

    Reapply "Add Event Food Item Relationship - start working on FoodItem Backend"
    This reverts commit 72aaf1e29ca1ca35c3a3301be9c4759fcbbe71cd.

---
+++ HouseChurchApi/Controllers/FoodItemController.cs
@@ -0,0 +1,36 @@
+﻿using HouseChurchApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using HouseChurchApi.Interfaces;
+
+
+namespace HouseChurchApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FoodItemController : ControllerBase
+    {
+        private readonly IPrayerRequestRepository _prayerRequestRepository;
+        public FoodItemController(IPrayerRequestRepository prayerRequestRepository)
+        {
+            _prayerRequestRepository = prayerRequestRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPrayerRequest()
+        {
+            var events = await _prayerRequestRepository.GetPrayerRequests();
+            return Ok(events);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePrayerRequest(int id)
+        {
+            await _prayerRequestRepository.DeletePrayerRequest(id);
+            return NoContent(); // 204 on successful deletion
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPrayerRequest([FromBody] PrayerRequest newPrayerRequest)
+        {
+            await _prayerRequestRepository.AddPrayerRequest(newPrayerRequest);
+            return CreatedAtAction(nameof(GetPrayerRequest), new { id = newPrayerRequest.Id }, newPrayerRequest);
+        }
+    }
+}
